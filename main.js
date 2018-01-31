28b24eab701fac83237e66d68a92530cb1a6c0c2
Hitesh Yadav <hhiteshyadav@gmail.com>
2018-01-31 18:58:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var url = 'http://hn.algolia.com/api/v1/search?query=javascript'; // Any API or URL where you want to get News
+    var url = 'https://hn.algolia.com/api/v1/search?query=javascript'; // Any API or URL where you want to get News
 
     $.getJSON(url,function(data){
         var current_news = '';
